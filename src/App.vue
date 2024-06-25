d333f58a84b0b68bd4735a832f8568bd9367f370
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-06-25 13:50:20 +0400

    [dev] refactoring

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ export default {
   data() {
     const { columns, cards } = getData();
     return { columns, cards };
-  },
+  }
 };
 </script>
 
--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -1,22 +1,30 @@
 <script>
-import { Kanban } from "@dhx/trial-kanban";
+import { Kanban, Toolbar } from "@dhx/trial-kanban";
 import "@dhx/trial-kanban/dist/kanban.css";
 
 export default {
   props: ["cards", "columns"],
 
   mounted() {
-    new Kanban(this.$refs.cont, {
+    const kanban = new Kanban(this.$refs.kanban_container, {
       cards: this.cards,
       columns: this.columns,
+      // other configuration properties
+    });
+
+    new Toolbar(this.$refs.toolbar_container, {
+      api: kanban.api
     });
   },
+
   unmounted() {
-    this.$refs.cont.innerHTML = "";
-  },
+    this.$refs.toolbar_container.innerHTML = "";
+    this.$refs.kanban_container.innerHTML = "";
+  }
 };
 </script>
 
 <template>
-  <div ref="cont" style="width: 100%; height: 100%"></div>
+    <div ref="toolbar_container"></div>
+    <div ref="kanban_container" style="width: 100%; height: 100%"></div>
 </template>
